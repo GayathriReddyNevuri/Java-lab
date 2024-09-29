dd49b339ce0b6595000f1ab7c89533120f945eed
GayathriReddyNevuri <gayathrinevuri545@gmail.com>
2024-09-29 22:01:38 +0530

    Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,11 +1,9 @@
 public class Invoice {
-    // Instance variables (package-private by default)
-    String partNumber;        // Part number of the item
-    String partDescription;   // Description of the item
-    int quantity;             // Quantity of items being purchased
-    double pricePerItem;      // Price of each item
+    String partNumber;
+    String partDescription;
+    int quantity;
+    double pricePerItem;
 
-    // Constructor to initialize the instance variables
     public Invoice(String partNumber, String partDescription, int quantity, double pricePerItem) {
         this.partNumber = partNumber;
         this.partDescription = partDescription;
@@ -13,12 +11,10 @@ public class Invoice {
         this.pricePerItem = pricePerItem;
     }
 
-    // Method to calculate the total invoice amount
     public double getInvoiceAmount() {
-        return quantity * pricePerItem; // Total amount = quantity * price per item
+        return quantity * pricePerItem;
     }
 
-    // Method to display invoice details
     public void displayInvoice() {
         System.out.println("Invoice Details:");
         System.out.println("Part Number: " + partNumber);
@@ -28,12 +24,8 @@ public class Invoice {
         System.out.printf("Total Invoice Amount: $%.2f%n", getInvoiceAmount());
     }
 
-    // Main method to run the application
     public static void main(String[] args) {
-        // Creating an Invoice object with example values
         Invoice invoice = new Invoice("A123", "Hammer", 5, 12.99);
-
-        // Displaying the invoice details
         invoice.displayInvoice();
     }
 }
